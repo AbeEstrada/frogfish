1e01153a80d6a126802d69bf49dc69d09ca7f479
Abraham Estrada <abraham.estrada@gmail.com>
2011-12-19 22:07:55 -0700

    instantiate new class

--- sys/mvc.php
+++ sys/mvc.php
@@ -4,5 +4,6 @@ require 'controller.php';
 require 'app/config.php';
 require 'app/model.php';
 
+$new_class = ucfirst($config['route']['default_controller']);
 require 'app/'.$config['route']['default_controller'].'.php';
-new $config['route']['default_controller'];
+new $new_class;

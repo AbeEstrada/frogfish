c0f745a19c714f32759358ddadf67108500aad8f
Abraham Estrada <abraham.estrada@gmail.com>
2011-12-19 21:46:41 -0700

    load multiple controllers

--- sys/mvc.php
+++ sys/mvc.php
@@ -2,5 +2,20 @@
 require 'load.php';
 require 'controller.php';
 require 'app/model.php';
-require 'app/application.php';
-new Application();
+
+$apps_dir = './app';
+if ($handle = opendir($apps_dir)) {
+    while (false !== ($file = readdir($handle))) {
+        $file_path = $apps_dir.'/'.$file;
+        $path_parts = pathinfo($file);
+        if ($path_parts['extension'] == 'php') {
+            $content = file_get_contents($file_path);
+            if (strpos($content, 'extends Controller') !== false) { // Find all controller files
+                require $file_path;
+                $new_class = ucfirst(substr($file, 0, strlen($file)-4));
+                new $new_class; // Create new controller
+            }
+        }
+    }
+    closedir($handle);
+}

3c3dd8bbd0d76ae89e1e6f2ccfc8e4034fbd8928
Abraham Estrada <abraham.estrada@gmail.com>
2011-12-19 22:53:52 -0700

    new url routing

--- sys/controller.php
+++ sys/controller.php
@@ -3,26 +3,8 @@ class Controller {
     public $load;
     public $model;
     
-    public $url;
-    public $urlparams = array();
-    
     function __construct() {
         $this->load = new Load();
         $this->model = new Model();
-        
-        if (isset($_SERVER['PATH_INFO'])) { // Nice URLs, get arguments
-            $this->url = explode('/', $_SERVER['PATH_INFO']);
-            $this->urlparams = array_slice($this->url, 2);
-        }
-        
-        if (isset($this->url[1]) && $this->url[1]) {
-            if (substr($this->url[1], 0, 1) != '_') {// Protect private functions starting with underscore
-                call_user_func_array(array($this, $this->url[1]), $this->urlparams); // Execute function from URL with params
-            } else {
-                header('HTTP/1.0 404 Not Found');
-            }
-        } else {
-            $this->index();
-        }
     }
 }

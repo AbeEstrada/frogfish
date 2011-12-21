d0585e7526ce2410044908b0c1fe2a71ecd61656
Abraham Estrada <abraham.estrada@gmail.com>
2011-12-21 12:30:11 -0700

    new routes example

--- index.php
+++ index.php
@@ -2,16 +2,22 @@
 require 'frogfish.php';
 
 class App extends Frogfish {
-    public function happy() {
+    public function home() {
         echo '<h1>It works!</h1>';
     }
     
     public function test() {
         echo 'This is a test';
     }
+    
+    public function hello($name, $age) {
+        echo 'Hello '.$name.', your age is '.$age;
+    }
+
 }
 
 new App(array(
-    '/' => 'happy',
-    '/test' => 'test',
+    '/' => 'home',
+    '/hello/$name/$age' => 'hello',
+    '/test' => 'test'
 ));

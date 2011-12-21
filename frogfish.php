9c17ed20c2829dd979bcb26f8e8927f5286721fe
Abraham Estrada <abraham.estrada@gmail.com>
2011-12-21 15:03:02 -0700

    removed methods duplicate name conflict, experimental input class (sanitize $_GET $_POST)

--- frogfish.php
+++ frogfish.php
@@ -1,42 +1,44 @@
 <?php
 class Frogfish {
-    private $config = array();
-    private $url = array();
-    private $routes = array();
-    private $action;
+    private $_config = array();
+    private $_url = array();
+    private $_routes = array();
+    private $_action;
+    
+    protected $input;
     
     public function __construct($routes, $config=array()) {
-        $this->config = $config;
-        $this->routes = $routes;
+        $this->_config = $config;
+        $this->_routes = $routes;
         
-        $this->router();
+        $this->input = new FrogfishInput();
+        
+        $this->_router();
     }
     
-    private function router() {
+    private function _router() {
         if (isset($_SERVER['PATH_INFO'])) {
             preg_match_all('/(\$?)(\w+|\*)/', $_SERVER['PATH_INFO'], $urls, PREG_PATTERN_ORDER);
-            $this->url = $urls[0];
+            $this->_url = $urls[0];
         }
-        
-        //print_r($this->url);
         
         $match = false;
         
-        if (!array_key_exists('/', $this->routes)) {
-            $this->routes['/'] = 'index';
+        if (!array_key_exists('/', $this->_routes)) {
+            $this->_routes['/'] = 'index';
         }
         
-        if (count($this->url) == 1 && empty($this->url[0])) {
+        if (count($this->_url) == 1 && empty($this->_url[0])) {
             $match = true;
-            $this->action = $this->routes['/'];
+            $this->_action = strtolower($this->_routes['/']);
         } else {
             $params = array();
             
-            foreach ($this->routes as $url => $action) {
+            foreach ($this->_routes as $url => $action) {
                 preg_match_all('/(\$?)(\w+|\*)/', $url, $route, PREG_PATTERN_ORDER);
                 $route = $route[0];
                 
-                if (count($this->url) <= count($route) || end($route) == '*') {
+                if (count($this->_url) <= count($route) || end($route) == '*') {
                     if (end($route) == '*') {
                         $last = count($route)-1;
                         unset($route[$last]);
@@ -44,17 +46,17 @@ class Frogfish {
                     
                     $matches = 0;
                     for ($i=0; $i < count($route); $i++) { 
-                        if (isset($this->url[$i]) && $route[$i] == $this->url[$i]) {
+                        if (isset($this->_url[$i]) && $route[$i] == $this->_url[$i]) {
                             $matches++;
-                        } else if (isset($this->url[$i]) && substr($route[$i], 0, 1) == '$') {
+                        } else if (isset($this->_url[$i]) && substr($route[$i], 0, 1) == '$') {
                             $matches++;
-                            array_push($params, $this->url[$i]);
+                            array_push($params, $this->_url[$i]);
                         }
                     }
                     
                     if (count($route) == $matches) {
                         $match = true;
-                        $this->action = $action;
+                        $this->_action = strtolower($action);
                         break;
                     }
                 }
@@ -62,17 +64,17 @@ class Frogfish {
         }
         
         if ($match) {
-            if (is_callable(array($this, $this->action)) && substr($this->action, 0, 1) != '_') {
-                call_user_func_array(array($this, $this->action), $params);
+            if (is_callable(array($this, $this->_action)) && substr($this->_action, 0, 1) != '_') {
+                call_user_func_array(array($this, $this->_action), $params);
             } else {
-                $this->error(404);
+                $this->_error(404);
             }
         } else {
-            $this->error(404);
+            $this->_error(404);
         }
     }
     
-    private function error($e) {
+    private function _error($e) {
         switch ($e) {
             case 404:
                 header('HTTP/1.0 404 Not Found');
@@ -83,5 +85,30 @@ class Frogfish {
                 header('HTTP/1.1 500 Internal Server Error');
                 break;
         }
+    }
+}
+
+class FrogfishInput {
+    private function xss_clean($input) {
+        if (is_array($input)) {
+            foreach($input as $var => $val) {
+                $output[$var] = xss_clean($val);
+            }
+        } else {
+            $input = trim($input);
+            $input = (get_magic_quotes_gpc()) ? stripslashes($input) : $input ;
+            //$input = mysql_escape_string($input);
+            $output = htmlentities($input, ENT_QUOTES, 'UTF-8');
+        }
+        return $output;
+    }
+    public function get($input) {
+        $output = $this->xss_clean($_GET[$input]);
+        return $output;
+    }
+    
+    public function post($input) {
+        $output = $this->xss_clean($_POST[$input]);
+        return $output;
     }
 }
